bdb100289d2bef7f15a56b5ab5e7ff98b39fc803
PratikMohite87 <59829840+PratikMohite87@users.noreply.github.com>
2023-03-26 23:54:27 +0530

    Add files via upload

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Interface : it is a pure abstract class that contains set of abstract method which must be implemented by the child classes.
+            //             access modifiers for all methods public you cannot change it.
+            //             all methods are public and abstract by default.
+            //             interface cannot be inhertie from other classes it can inherit from interface.
+            //             interface can only have Non-Static Events, Abstract Methods, Non-Static autoimplemented properties.
+        }
+    }
+}
