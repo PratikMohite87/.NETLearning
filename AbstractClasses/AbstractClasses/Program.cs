4ab75465cd5fe781a44e172cf2502d8901a27d3a
PratikMohite87 <59829840+PratikMohite87@users.noreply.github.com>
2023-03-26 23:54:45 +0530

    Add files via upload

---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Abstract Class : its is a parent class for which we can`t create object but we can create child class.
+            //                  Main intention is to provide common set of fields & methods to all its child classes of specific group.
+            //                  it can inherit from other class or interface.
+
+            // Abstract Method : are declared in parent class with "abstract keyword"; implemented in child classes with "override" keyword.
+            //                   it is used when parent class don`t want to provide the definition of member,
+            //                   but it wants to let child classes to implement method.
+        }
+    }
+}
